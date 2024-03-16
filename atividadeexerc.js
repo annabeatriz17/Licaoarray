44e4282fa3579d23b10a7c947f6040d7ad7dbda8
Anna Beatriz <annavalentim.tds1.senai@gmail.com>
2024-03-16 11:13:49 -0300

    retirei o Labrador, Golden e o Bulldog para passear

--- atividadeexerc.js
+++ atividadeexerc.js
@@ -9,8 +9,15 @@ console.log(refrigerantes);
 
 //método pop;
 //Remove refrigerantes do final da lista;
-let refrigerantes = ['Coca-Cola', 'Pepsi' 'Guaraná']
+let refrigerante = ['Coca-Cola', 'Pepsi', 'Guaraná']
 let ultimoRefrigerante = refrigerantes.pop;
 console.log (ultimoRefrigerante); //Removeu da lista o Guaraná;
 
-console.log(refrigerantes); // Remoceu da lista a Coca-Cola e a Pepsi;
+console.log(refrigerantes); // Removeu da lista a Coca-Cola e a Pepsi;
+
+//Retirando o primeiro cachorro da lista para passear;
+let cachorros = ['Labrador', 'Golden', 'Bulldog'];
+let primeiroCachorro = cachorros.shift;
+console.log(primeiroCachorro); //Retirei o Labrador para passear;
+
+console.log(cachorros); //Retirei o Golden e o Bulldog para passear;
