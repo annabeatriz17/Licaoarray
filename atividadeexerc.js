8907e5f00f45475434d0b9e78c35d529224cf432
Anna Beatriz <annavalentim.tds1.senai@gmail.com>
2024-03-16 11:03:16 -0300

    removi o Guaraná, Coca-Cola e a Pepsi da lista

---
+++ atividadeexerc.js
@@ -0,0 +1,16 @@
+//método push;
+//Adicionando novos refrigerantes ao final da lista;
+let refrigerantes = ['Coca-Cola', 'Pepsi'];
+refrigerantes.push('Guaraná'); 
+console.log(refrigerantes); 
+
+refrigerantes.push('Fanta', 'Sprite');
+console.log(refrigerantes); 
+
+//método pop;
+//Remove refrigerantes do final da lista;
+let refrigerantes = ['Coca-Cola', 'Pepsi' 'Guaraná']
+let ultimoRefrigerante = refrigerantes.pop;
+console.log (ultimoRefrigerante); //Removeu da lista o Guaraná;
+
+console.log(refrigerantes); // Remoceu da lista a Coca-Cola e a Pepsi;
